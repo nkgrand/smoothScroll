e808a19e9aff144bf4cc9e2f530f66f128c54bed
Andrey Grigoriev <nkgrand25@gmail.com>
2021-07-12 20:41:01 +0300

    added translate

--- scrpt.js
+++ scrpt.js
@@ -1,15 +1,15 @@
-// Найти все ссылки начинающиеся с #
+// Find all links starting with #
 const anchors = document.querySelectorAll('a[href*="#"]');
 
-// Цикл по всем ссылкам
+// Loop through all links
 for (let anchor of anchors) {
   anchor.addEventListener("click", function (event) {
-    event.preventDefault(); // Предотвратить стандартное поведение ссылок
-    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    event.preventDefault(); // Prevent standard link behavior
+    // The href attribute of the link, if it does not exist then go to the body (not smoothly to the top)
     const blockID = anchor.hasAttribute("href")
       ? anchor.getAttribute("href")
       : "body";
-    // Плавная прокрутка до элемента с id = href у ссылки
+    // Smooth scrolling to the element with id = href of the link
     document.querySelector(blockID).scrollIntoView({
       behavior: "smooth",
       block: "start",
